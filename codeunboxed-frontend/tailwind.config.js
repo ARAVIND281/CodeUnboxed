a5c1efbc2cd8e97433668f1bdd9a52b9d46c1021
Aravind S <aravind@inbo.tech>
2025-07-05 00:12:22 +0530

    chore: update package dependencies and migrate Tailwind CSS configuration to new format

--- codeunboxed-frontend/tailwind.config.js
+++ codeunboxed-frontend/tailwind.config.js
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+// import type { Config } from "tailwindcss";
 
 export default {
 	darkMode: ["class"],
@@ -111,4 +111,4 @@ export default {
 		}
 	},
 	plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+}
